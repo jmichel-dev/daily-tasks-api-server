a70770ab587e98b2cd9cf121980f8753c39213a1
Dev JM <Jean Santos>
2023-09-04 08:37:55 -0300

    included auth route in app definition

--- daily_tasks_server/src/app.py
+++ daily_tasks_server/src/app.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 
+from daily_tasks_server.src.controllers.http.router import api_router
+
 
 def create_app() -> FastAPI:
     app = FastAPI()
+
+    app.include_router(api_router, prefix="/api")
 
     return app
