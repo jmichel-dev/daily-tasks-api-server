a9eded087d669dce8e31beaea1f43fd2420c6067
Dev JM <Jean Santos>
2023-09-05 19:19:12 -0300

    defining database session, and user input for signup

--- daily_tasks_server/src/controllers/http/v1/auth/auth_user.py
+++ daily_tasks_server/src/controllers/http/v1/auth/auth_user.py
@@ -1,4 +1,8 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, Depends
+
+from daily_tasks_server.src.models import UserSignupModel
+from daily_tasks_server.src.config.database import DatabaseInterface
+from daily_tasks_server.src.config.database import DatabaseSession
 
 router = APIRouter()
 
@@ -7,5 +11,8 @@ router = APIRouter()
     '',
     status_code=status.HTTP_201_CREATED
 )
-async def signup() -> dict:
+async def signup(
+        user: UserSignupModel,
+        session: DatabaseInterface = Depends(DatabaseSession)
+) -> dict:
     return {"msg": "Hello, World"}
