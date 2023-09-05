5e1f0a1fcaedb59c1694dbe99cb3de29c20cf85b
Dev JM <Jean Santos>
2023-09-05 08:40:40 -0300

    better code legibility

--- daily_tasks_server/src/controllers/http/v1/auth/auth_user.py
+++ daily_tasks_server/src/controllers/http/v1/auth/auth_user.py
@@ -3,6 +3,9 @@ from fastapi import APIRouter, status
 router = APIRouter()
 
 
-@router.post('', status_code=status.HTTP_201_CREATED)
+@router.post(
+    '',
+    status_code=status.HTTP_201_CREATED
+)
 async def signup() -> dict:
     return {"msg": "Hello, World"}
