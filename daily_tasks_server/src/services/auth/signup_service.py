0d6dc3c4e1ed547c5b727ddd5e89355fede3df18
Dev JM <Jean Santos>
2023-09-06 08:20:29 -0300

    defined user signup service

---
+++ daily_tasks_server/src/services/auth/signup_service.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+
+from daily_tasks_server.src.entity import User
+from daily_tasks_server.src.models import UserSignupModel
+from daily_tasks_server.src.config.database import DatabaseInterface
+
+
+class SignupService:
+
+    def __init__(self, db_session) -> None:
+        self.db_session = db_session
+
+    def execute(self, user_request: UserSignupModel) -> None:
+        with self.db_session as session:
+            sql = ("INSERT INTO person(first_name, last_name, email, password, active_email, enable, created_at) "
+                   "VALUES(%s, %s, %s, %s, %s, %s, %s)")
+
+            user = User()
+            user.change_first_name(user_request.first_name)
+            user.change_last_name(user_request.last_name)
+            user.change_email(user_request.email)
+            user.encrypt_password(user_request.password)
+            user.change_created_at(datetime.utcnow())
+            user.enable_user()
+
+            data = (
+                user.first_name,
+                user.last_name,
+                user.email,
+                user.password,
+                user.active_email,
+                user.enable,
+                user.created_at,
+            )
+
+            cursor = session.cursor()
+            cursor.execute(sql, data)
+            session.commit()
