7550a3f7e587846dc93a7cc307638bb13aab22ba
Dev JM <Jean Santos>
2023-09-07 12:33:01 -0300

    service to send emails

--- daily_tasks_server/src/services/mail/send_email_service.py
+++ daily_tasks_server/src/services/mail/send_email_service.py
@@ -1,3 +1,22 @@
-class SendEmail:
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
-    @
+from daily_tasks_server.src.config import Config
+
+
+class SendEmailService:
+
+    @staticmethod
+    def send(subject: str, html_message: str, to: str) -> None:
+        message = MIMEMultipart("alternative")
+        message["Subject"] = subject
+        message["From"] = Config.EMAIL_SENDER
+        message["To"] = to
+
+        mimed_html_message = MIMEText(html_message, "html")
+        message.attach(mimed_html_message)
+
+        with smtplib.SMTP(Config.EMAIL_HOST, Config.EMAIL_PORT) as server:
+            server.login(Config.EMAIL_LOGIN, Config.EMAIL_PASSWORD)
+            server.sendmail(Config.EMAIL_SENDER, to, message.as_string())
