78f289ea3a592ab1ebbed6cd924c42891fdee966
Dev JM <Jean Santos>
2023-09-07 12:34:01 -0300

    service to send notification to email activation

--- daily_tasks_server/src/services/notification/activate_user_email_notification_service.py
+++ daily_tasks_server/src/services/notification/activate_user_email_notification_service.py
@@ -1,9 +1,12 @@
+from pathlib import Path
+
 from daily_tasks_server.src.config import Config
 from daily_tasks_server.src.models import UserSignupModel
 from daily_tasks_server.src.services import JWTService
+from daily_tasks_server.src.services import SendEmailService
 
 
-class ActivateUserEmailNotification:
+class ActivateUserEmailNotificationService:
 
     @staticmethod
     async def notify(user: UserSignupModel) -> None:
@@ -11,3 +14,12 @@ class ActivateUserEmailNotification:
         expiration = Config.JWT_ACTIVATE_EMAIL_TOKEN_EXPIRATION
 
         token = JWTService.generate(payload, expiration)
+        html = Path(Config.MAIL_TEMPLATE_ACTIVATE_USER_EMAIL).read_text()
+
+        html_message = html.format(
+            first_name=user.first_name,
+            host=Config.APP_HOST,
+            token=token
+        )
+
+        SendEmailService.send("Daily Tasks App: Please, confirm your email", html_message, user.email)
