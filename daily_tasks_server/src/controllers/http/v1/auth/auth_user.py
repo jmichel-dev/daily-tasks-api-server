8e01465036f35af2b0da2dac09bc2383e6bdce7a
Dev JM <Jean Santos>
2023-09-09 22:45:08 -0300

    route for activate user email

--- daily_tasks_server/src/controllers/http/v1/auth/auth_user.py
+++ daily_tasks_server/src/controllers/http/v1/auth/auth_user.py
@@ -1,3 +1,5 @@
+import json
+
 from fastapi import APIRouter, status, Depends, BackgroundTasks
 
 from daily_tasks_server.src.models import UserSignupModel
@@ -5,13 +7,16 @@ from daily_tasks_server.src.config.database import DatabaseInterface
 from daily_tasks_server.src.config.database import DatabaseSession
 from daily_tasks_server.src.services import SignupService
 from daily_tasks_server.src.services import ActivateUserEmailNotificationService
+from daily_tasks_server.src.services import JWTService
+from daily_tasks_server.src.services import ConfirmEmailService
 
 router = APIRouter()
 
 
 @router.post(
-    '',
-    status_code=status.HTTP_201_CREATED
+    '/signup/',
+    status_code=status.HTTP_201_CREATED,
+    name="Signup user"
 )
 async def signup(
         background_task: BackgroundTasks,
@@ -22,3 +27,19 @@ async def signup(
     signup_service.execute(user)
 
     background_task.add_task(ActivateUserEmailNotificationService.notify, user)
+
+
+@router.post(
+    "/activate/{token}",
+    status_code=status.HTTP_200_OK,
+    name="Activate user email"
+)
+async def activate(
+        token: str,
+        db: DatabaseInterface = Depends(DatabaseSession)
+) -> None:
+    payload = JWTService.verify(token)
+
+    user = payload["payload"]["email"]
+    confirm_email_service = ConfirmEmailService(db_session=db.get_session())
+    confirm_email_service.execute(user)
