08930c3f7e3b06933dd58d8c7337193a2d3199a5
Dev JM <Jean Santos>
2023-09-09 22:47:41 -0300

    removed unused imports

--- daily_tasks_server/src/services/auth/signup_service.py
+++ daily_tasks_server/src/services/auth/signup_service.py
@@ -2,7 +2,6 @@ from datetime import datetime
 
 from daily_tasks_server.src.entity import User
 from daily_tasks_server.src.models import UserSignupModel
-from daily_tasks_server.src.config.database import DatabaseInterface
 
 
 class SignupService:
