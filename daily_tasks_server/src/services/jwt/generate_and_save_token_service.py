a568738a19208ba2ff34fb637ccab5a60e43b902
Dev JM <Jean Santos>
2023-09-13 08:56:24 -0300

    new token database

---
+++ daily_tasks_server/src/services/jwt/generate_and_save_token_service.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+
+from daily_tasks_server.src.config import Config
+from daily_tasks_server.src.services import JWTService
+
+
+class GenerateAndSaveTokenService:
+
+    def __init(self, db_session) -> None:
+        self.db_session = db_session
+
+    def execute(self, email: str) -> str:
+        payload = {
+            "email": email
+        }
+
+        token = JWTService.generate(payload=payload, expiration=Config.JWT_ACTIVATE_EMAIL_TOKEN_EXPIRATION)
+        created_at = datetime.utcnow()
+
+        sql = "INSERT INTO tokens(token, created_at) VALUES(%s, %s)"
+
+        data = (token, created_at)
+
+        with self.db_session as session:
+            cursor = session.cursor()
+            cursor.execute(sql, data)
+            session.commit()
+
+        return token
+
--- docker/database/create_todo_database.sql
+++ docker/database/create_todo_database.sql
@@ -5,10 +5,18 @@ CREATE TABLE IF NOT EXISTS person(
     id uuid DEFAULT gen_random_uuid() PRIMARY KEY,
     first_name VARCHAR(80) NOT NULL,
     last_name VARCHAR(80) NOT NULL,
-    email VARCHAR(150) NOT NULL,
+    email VARCHAR(150) UNIQUE NOT NULL,
     password VARCHAR NOT NULL,
     active_email BOOL DEFAULT false,
     enable BOOL DEFAULT true,
+    created_at timestamp default CURRENT_TIMESTAMP not null,
+    updated_at timestamp default CURRENT_TIMESTAMP not null
+);
+
+CREATE TABLE IF NOT EXISTS tokens(
+    id uuid DEFAULT gen_random_uuid() PRIMARY KEY,
+    token VARCHAR NOT NULL,
+    valid BOOL DEFAULT true,
     created_at timestamp default CURRENT_TIMESTAMP not null,
     updated_at timestamp default CURRENT_TIMESTAMP not null
 );
