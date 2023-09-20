af445b24a1e3c8fb75ff3ac669c563a58080cef9
Dev JM <Jean Santos>
2023-09-19 22:06:59 -0300

    fix: check if the current password is matching with the old password request

--- daily_tasks_server/src/controllers/auth/change_password_controller.py
+++ daily_tasks_server/src/controllers/auth/change_password_controller.py
@@ -1,7 +1,10 @@
+from fastapi import HTTPException, status
+
 from daily_tasks_server.src.config.database import DatabaseInterface
 from daily_tasks_server.src.models.auth.change_password_model import ChangePasswordModel
 from daily_tasks_server.src.services import JWTService, VerityJWTTokenDatabaseService, DisableTokenService
 from daily_tasks_server.src.services.auth.change_password_by_email_service import ChangePasswordByEmailService
+from daily_tasks_server.src.services.auth.compare_user_password_service import CompareUserPasswordService
 
 
 class ChangePasswordController:
@@ -11,6 +14,15 @@ class ChangePasswordController:
         with db.get_session() as session:
             payload = JWTService.verify(password_request.token)
             email = payload["payload"]["email"]
+
+            compare_user_password_service = CompareUserPasswordService(session)
+            valid_password = compare_user_password_service.execute(email, password_request.old_password)
+
+            if not valid_password:
+                raise HTTPException(
+                    status_code=status.HTTP_400_BAD_REQUEST,
+                    detail="Email/password invalid"
+                )
 
             verify_token_service = VerityJWTTokenDatabaseService(session)
             verify_token_service.execute(password_request.token)
